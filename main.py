f63a47786d4433e040835cc1c68b46f331afb1b4
Nolan1206 <2022110289@mail.scuec.edu.cn>
2023-03-13 13:07:20 +0800

    first commit2

--- main.py
+++ main.py
@@ -14,3 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
